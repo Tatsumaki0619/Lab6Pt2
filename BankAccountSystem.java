747f2154e776145abc882b6f41a46dbf59b89675
Tatsumaki0619 <tatsumakiK6@gmail.com>
2025-03-25 11:07:16 -0700

    Done

--- BankAccountSystem.java
+++ BankAccountSystem.java
@@ -1,61 +1,133 @@
 
 /**
- * Write a description of class BankAccountSystem here.
+ * This program helps the user calculate and generate blueprint of the user's bank account information, including
+ * interest rate, current balance, current interest, total deposit, total withdraw and total interest.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Qianyi Li
+ * @version v1.0
+ * @since 3/20/2025
  */
+
 public class BankAccountSystem
 {
     private double balance, deposit, withdraw, interest, monthlyIntrate;
-    
-    public void BankAccount(){
+
+    /**
+     * This is a basic constructor and will set the monthly interest rate to 0.035/12, and set the rest of the information to 0.
+     * (The user should input the yearly interest rate and the program will convert it to monthly rate.
+     * The user's input should look like i.e.: 3.5(same as 3.5%))
+     */
+    public void BankAccount() //no-arg constructor
+    { 
         monthlyIntrate = 0.035/12;
     }
-    
-    public void BankAccount(double inBalance, double intRate){
-        
+
+    /**
+     * This is a complete constructor and will set the balance, monthly interest rate based on the user's inputs
+     * (The user should input the yearly interest rate and the program will convert it to monthly rate.
+     * The user's input should look like i.e.: 3.5(same as 3.5%))
+     * @param inBalance This is the starting balance the user entered
+     * @param inRate This is the year interest rate the user entered
+     */
+    public void BankAccount(double inBalance, double inRate){
+        balance = inBalance;
+        monthlyIntrate = inRate/1200;
+        interest = 0;
     }
-    
-    public void BankAccount(String inBalance, String intRate){
-        
-    }
-    
+
+    /**
+     * This method will store the user's monthly deposit, and accumulate it to the total deposit and change the user's balance.
+     * @param add This is user's monthly deposit based on the user's input.
+     */
     public void makeDeposit(double add){
-        
-    }
-    
-    public void makeDeposit(String add){
         balance += add;
+        deposit += add;
     }
-    
-    public void MakeWithdraw(String sub){
-        
-    }
-    
-    public void MakeWithdra(double sub){
+
+    /**
+     * This method will store the user's monthly withdraw, and accumulate it to the total withdraw and change the user's balance.
+     * @param sub This is user's monthly withdraw based on the user's input.
+     */
+    public void makeWithdraw(double sub){
         balance -= sub;
+        withdraw += sub;
     }
-    
+
+    /**
+     * This method will calculate the user's monthly interest, and accumulate to the total interest.
+     * If the current balance is 0, this month's interest will be 0. 
+     */
     public void calcInterest(){
-        interest = monthlyIntrate * balance; 
+        if (balance > 0){
+            interest += monthlyIntrate * balance;
+            balance += monthlyIntrate * balance;
+        }
+        else{
+            interest += 0;
+        }
     }
-    
+
+    /**
+     * This method will return the user's current balance.
+     * @return This is the user's current balance.
+     */
     public double getBalance(){
-        balance = balance - withdraw + interest + deposit;
         return balance;
     }
-    
+
+    /**
+     * This method will return the user's current total deposit.
+     * @return This is the user's current total deposit.
+     */
     public double getDeposit(){
-        deposit +=   
-        return
+        return deposit;
     }
-    
-    public double getWithdraw(double dep){
+
+    /**
+     * This method will return the user's current total withdraw.
+     * @return This is the user's current total withdraw.
+     */
+    public double getWithdraw(){
+        return withdraw;
     }
-    
-    public double getInterest(){
-        return
+
+    /**
+     * This method will return the user's current total interest.
+     * @return This will be the user's current total interest.
+     */
+    public double getInterests(){
+        return interest;
     }
-    
+
+    /**
+     * This is a complete constructor and will set the balance, monthly interest rate based on the user's inputs
+     * (The user should input the yearly interest rate and the program will convert it to monthly rate.
+     * The user's input should look like i.e.: 3.5(same as 3.5%))
+     * @param inBalance This is the starting balance the user entered
+     * @param inRate This is the year interest rate the user entered
+     */
+    public void BankAccount(String inBalance, String inRate){
+        balance = Double.parseDouble(inBalance);
+        monthlyIntrate = Double.parseDouble(inRate)/1200;        
+    }
+
+    /**
+     * This method will store the user's monthly deposit, and accumulate it to the total deposit and change the user's balance.
+     * @param add This is user's monthly deposit based on the user's input.
+     */    
+    public void makeDeposit(String add){
+        double temp = Double.parseDouble(add);
+        balance += temp;
+        deposit += temp;
+    }
+
+    /**
+     * This method will store the user's monthly withdraw, and accumulate it to the total withdraw and change the user's balance.
+     * @param sub This is user's monthly withdraw based on the user's input.
+     */
+    public void makeWithdraw(String sub){
+        double temp = Double.parseDouble(sub);
+        balance -= temp;
+        withdraw += temp;
+    }
 }
--- Lab6Pt2BankAccount.java
+++ Lab6Pt2BankAccount.java
@@ -1,38 +1,93 @@
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 /**
- * Write a description of class Lab6Pt2BankAccount here.
+ * This is a program that can help users calculate the user's bank account balance and interest.
+ * The program can also record and accumulate the user's monthly deposit and withdraw.
+ * The user can choose to sue a console or a GUI to get the output.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Qianyi Li
+ * @version v1.0
+ * @since 3/20/2025
  */
 public class Lab6Pt2BankAccount
 {
     public static void main(String[] args){
-        int mode, time;
-        double temp;
-        
+        int mode;
         BankAccountSystem bank = new BankAccountSystem();
         Scanner keyboard = new Scanner(System.in);
         do{
             System.out.print("Press 1 to use console or 2 to use GUI: ");
             mode = keyboard.nextInt();
-            System.out.println();
-        }while(mode != 1 || mode != 2);
-        System.out.print("What is your starting balance: $");
-        temp = keyboard.nextDouble();
-        bank.getBalance(temp);
-        System.out.print("What is your interest rate? (ie wnter 3.5 for 3.5%): $");
-        temp = keyboard.nextDouble();
-        bank.getInterest(temp);
-        System.out.print("How many month do you want to calculate? ");
-        time = keyboard.nextInt();
-        System.out.println("===============================");
+        }while(!(mode == 1 || mode == 2));
+        System.out.println();
         if(mode == 1){
-            for(int i = 1; i <= time; i ++){
-                int temDeposit, temWithdraw;
-            }
+            useConsole();
         }
-        
+        else{
+            useGUI();
+        }
+        System.exit(0);
+    }
+
+    /**
+     * This method can provide the user's bank account information in a console format,
+     * including balance, total deposit, withdraw and total interest based
+     * on the user's input value of starting balance and interest rate.
+     */
+    public static void useConsole(){
+        double balance, intRate;
+        int counter;
+        BankAccountSystem bank = new BankAccountSystem();
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("What is your starting balance: $");
+        balance = keyboard.nextDouble();
+        System.out.print("What is your interest rate? (ie enter 3.5 for 3.5%): ");
+        intRate = keyboard.nextDouble();
+        System.out.print("How many month do you want to calculate? ");
+        counter = keyboard.nextInt();
+        System.out.println("===============================");
+        bank.BankAccount(balance, intRate);
+        for(int i=1;i<=counter;i++){
+            System.out.print("Enter Month " + i + " deposits $");
+            bank.makeDeposit(keyboard.nextDouble());
+            System.out.print("Enter Month " + i + " withdraws $");
+            bank.makeWithdraw(keyboard.nextDouble());
+            bank.calcInterest();
+        }
+        System.out.println();
+        System.out.printf("Ending balance: $%,.2f\n", bank.getBalance());
+        System.out.printf("Total deposits: $%,.2f\n", bank.getDeposit());
+        System.out.printf("Total withdraw: $%,.2f\n", bank.getWithdraw());
+        System.out.printf("Total interest: $%,.2f\n", bank.getInterests());
+
+    }
+
+    /**
+     * This method can provide the user's bank account information in a GUI format,
+     * including balance, total deposit, withdraw and total interest based
+     * on the user's input value of starting balance and interest rate.
+     */
+    public static void useGUI(){
+        String balance, intRate, counter;
+        int counter1;
+        BankAccountSystem bank = new BankAccountSystem();
+
+        balance = JOptionPane.showInputDialog("What is your starting balance: $");
+        intRate = JOptionPane.showInputDialog("What is your interest rate? (ie enter 3.5 for 3.5%): ");
+        counter = JOptionPane.showInputDialog("How many month do you want to calculate? ");
+        counter1 = Integer.parseInt(counter);
+        System.out.println("===============================");
+        bank.BankAccount(balance, intRate);
+        for(int i=1;i<=counter1;i++){        
+            bank.makeDeposit(JOptionPane.showInputDialog("Enter Month " + i + " deposits $"));
+            bank.makeWithdraw(JOptionPane.showInputDialog("Enter Month " + i + " withdraws $"));
+            bank.calcInterest();
+        }
+        String print;
+        print = String.format("Ending balance: $%,.2f\n" + 
+            "Total deposits: $%,.2f\n" + "Total withdraw: $%,.2f\n" + 
+            "Total interest: $%,.2f\n", bank.getBalance(), bank.getDeposit(), bank.getWithdraw(), bank.getInterests());
+        JOptionPane.showMessageDialog(null,print);
     }
 }
